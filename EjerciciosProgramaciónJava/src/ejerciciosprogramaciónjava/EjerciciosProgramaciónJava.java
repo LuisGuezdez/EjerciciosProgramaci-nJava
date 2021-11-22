85730fc7ad525717b13cebca3b3a66d4b1538ea9
LuisGuezdez <isguezdez@gmail.com>
2021-11-22 19:59:36 +0100

    Creado proyecto Java

---
+++ EjerciciosProgramaciónJava/src/ejerciciosprogramaciónjava/EjerciciosProgramaciónJava.java
@@ -0,0 +1,22 @@
+/*
+ * Proyecto EjerciciosProgramaci_nJava - Archivo EjerciciosProgramaciónJava.java - Compañía DAW
+ * Licencia Creative Commons BY-NC-SA 4.0
+ * https://creativecommons.org/licenses/by-nc-sa/4.0/
+ */
+package ejerciciosprogramaciónjava;
+
+/**
+ *
+ * @author Luis Rodríguez <isguezdez@gmail.com>
+ */
+public class EjerciciosProgramaciónJava 
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

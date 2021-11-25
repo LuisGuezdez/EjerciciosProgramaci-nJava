7f705affd2ee07b2ac05b23e4354dd6a0ad23e78
LuisGuezdez <isguezdez@gmail.com>
2021-11-25 16:59:52 +0100

    Falta hacer la ultima opcion y comprobar que todo funciona

--- EjerciciosProgramaciónJava/src/ejerciciosprogramaciónjava/UD5Calculadora.java
+++ EjerciciosProgramaciónJava/src/ejerciciosprogramaciónjava/UD5Calculadora.java
@@ -80,12 +80,12 @@ public class UD5Calculadora
                 
             //area circulo
             case 7:
-                sout();
+                sout(areaCir());
                 break;
                 
             //funciones trigono
             case 8:
-                sout();
+                sout(trigon());
                 break;
                 
             //salir
@@ -104,11 +104,37 @@ public class UD5Calculadora
     {
         Scanner cin = new Scanner(System.in);
         double num;
-        do
+        //1 a 4
+        if(min == 1)
         {
-            System.out.print("Dime " + t + ": ");
-            num = cin.nextDouble();
-        }while(num < min && num > max);
+            //B de division
+            if(max == 1)
+            {
+                do
+                {
+                    System.out.print("Dime " + t + ": ");
+                    num = cin.nextDouble();
+                }while(num == 0);
+            }
+            //todos los demás
+            else
+            {
+                System.out.print("Dime " + t + ": ");
+                num = cin.nextDouble();
+            }
+        }
+        //5 a 8
+        else
+        {
+            do
+            {
+                System.out.print("Dime " + t + ": ");
+                num = cin.nextDouble();
+                if(num < min || num > max)
+                    System.out.println("Introduce valores válidos.");
+            }while(num < min || num > max);
+        }
+        
         
         return num;
     }
@@ -148,7 +174,7 @@ public class UD5Calculadora
         double y, x, resp;  
         System.out.println("B no puede ser 0");
         x = pedir(1, 0, "A");
-        y = pedir(1, 0, "B");
+        y = pedir(1, 1, "B");
         
             
            
@@ -161,13 +187,10 @@ public class UD5Calculadora
     public static double areaRect()
     {
         double y, x, resp;
-        do
-        {    
-            x = pedir(0, 1000000, "la base");
-            y = pedir(0, 1000000, "la altura");
-            
-        }while(y < 0 && y > 1000000 || x < 0 && x > 1000000 );
-        
+        System.out.println("Valor máx.: 1000000");
+        System.out.println("Valor mín.: 0");
+        x = pedir(0, 1000000, "la base");
+        y = pedir(0, 1000000, "la altura");
         resp = x * y;
         
         return resp;
@@ -176,13 +199,10 @@ public class UD5Calculadora
     public static double areaTri()
     {
         double y, x, resp;
-        do
-        {    
-            x = pedir(0, 1000000, "A");
-            y = pedir(0, 1000000, "B");
-            
-        }while(y < 0 && y > 1000000 || x < 0 && x > 1000000 );
-        
+        System.out.println("Valor máx.: 1000000");
+        System.out.println("Valor mín.: 0");
+        x = pedir(0, 1000000, "A");
+        y = pedir(0, 1000000, "B");
         resp = (x * y / 2);
         
         return resp;
@@ -191,14 +211,29 @@ public class UD5Calculadora
     public static double areaCir()
     {
         double x, resp;
-        do
-        {    
-            x = pedir(-360, 360, "el radio");
-            
-        }while(x < - && x > 1000000 );
+        System.out.println("Valor máx.: 360");
+        System.out.println("Valor mín.: -360");
+        x = pedir(-360, 360, "el radio");
         
-        resp = x * y;
+        resp = Math.PI * (x * x);
         
         return resp;
+    }
+    
+    public static double trigon()
+    {
+        double x, y, z, resp;
+        System.out.println("Valor máx.: 1000000");
+        System.out.println("Valor mín.: 0");
+        x = pedir(0, 1000000, "el seno");
+        y = pedir(0, 1000000, "el coseno");
+        z = pedir(0, 1000000, "la tangente");
+        
+        resp = Math.sin(x);
+        resp = Math.cos(y);
+        resp = Math.tan(z);
+        
+        return resp;
+    }
 }
     
